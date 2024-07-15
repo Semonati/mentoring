793c6c26a5a358f1299ea1705a719c6daec59c6e
Semonati <93730539+Semonati@users.noreply.github.com>
2024-07-15 13:52:03 +0300

    Update blockController.js

--- server/controllers/blockController.js
+++ server/controllers/blockController.js
@@ -4,17 +4,21 @@ const Blocks = require("../DB/mongodb/blocks");
 const DB = process.env.DB || "MONGODB";
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  if (DB === "MONGODB") {
-    try {
-      const blocks = await Blocks.find();
-      res.status(200).send(blocks);
-    } catch (error) {
-      error.status = 404;
-      return Promise.reject(error);
-    }
-  }
-});
+// router.get("/", async (req, res) => {
+//   if (DB === "MONGODB") {
+//     try {
+//       const blocks = await Blocks.find();
+//       res.status(200).send(blocks);
+//     } catch (error) {
+//       error.status = 404;
+//       return Promise.reject(error);
+//     }
+//   }
+// });
+
+router.get("/", (req, res) => {
+    res.json("Hello");
+})
 
 router.get("/:id", async (req, res) => {
   if (DB === "MONGODB") {
