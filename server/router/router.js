e3c935736eb84db382791caf42f51797a9a69872
Semonati <93730539+Semonati@users.noreply.github.com>
2024-07-15 13:49:33 +0300

    Update router.js

--- server/router/router.js
+++ server/router/router.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const blocksControllers = require("../controllers/blockController");
+// const blocksControllers = require("../controllers/blockController");
 
 const router = express.Router();
 
-router.use("/blocks", blocksControllers);
+// router.use("/blocks", blocksControllers);
+router.get("/", (req, res) => {
+    res.json("Hello");
+})
+
 
 module.exports = router;
