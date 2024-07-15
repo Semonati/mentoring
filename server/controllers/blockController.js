2438c5af6bdfdf9e5f92540d9a6ea974e11a2330
Natan Semo <justsemo9@gmail.com>
2024-07-15 14:26:48 +0300

    Update controller

--- server/controllers/blockController.js
+++ server/controllers/blockController.js
@@ -4,23 +4,19 @@ const Blocks = require("../DB/mongodb/blocks");
 const DB = process.env.DB || "MONGODB";
 const router = express.Router();
 
-// router.get("/", async (req, res) => {
-//   if (DB === "MONGODB") {
-//     try {
-//       const blocks = await Blocks.find();
-//       res.status(200).send(blocks);
-//     } catch (error) {
-//       error.status = 404;
-//       return Promise.reject(error);
-//     }
-//   }
-// });
+const getBlocks = router.get("/", async (req, res) => {
+  if (DB === "MONGODB") {
+    try {
+      const blocks = await Blocks.find();
+      res.status(200).send(blocks);
+    } catch (error) {
+      error.status = 404;
+      return Promise.reject(error);
+    }
+  }
+});
 
-router.get("/", (req, res) => {
-    res.json("Hello");
-})
-
-router.get("/:id", async (req, res) => {
+const getBlocksById = router.get("/:id", async (req, res) => {
   if (DB === "MONGODB") {
     try {
       const { id } = req.params;
@@ -33,4 +29,39 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-module.exports = router;
+// router.get("/", async (req, res) => {
+//   if (DB === "MONGODB") {
+//     try {
+//       const blocks = await Blocks.find();
+//       res.status(200).send(blocks);
+//     } catch (error) {
+//       error.status = 404;
+//       return Promise.reject(error);
+//     }
+//   }
+// });
+
+// const test = () => {
+//   try {
+//     router.get("/", (req, res) => {
+//       res.json("Hello");
+//     });
+//   } catch (error) {}
+// };
+
+// router.get("/:id", async (req, res) => {
+//   if (DB === "MONGODB") {
+//     try {
+//       const { id } = req.params;
+//       const blocks = await Blocks.findById(id);
+//       res.status(200).send(blocks);
+//     } catch (error) {
+//       error.status = 404;
+//       return Promise.reject(error);
+//     }
+//   }
+// });
+
+// module.exports = router;
+exports.getBlocksById = getBlocksById;
+exports.getBlocks = getBlocks;
--- server/router/router.js
+++ server/router/router.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const blocksControllers = require("../controllers/blockController");
+// const blocksControllers = require("../controllers/blockController");
+const { getBlocks, getBlocksById } = require("../controllers/blockController");
 
 const router = express.Router();
 
-router.use("/",blocksControllers);
+// router.use("/",blocksControllers);
+router.get("/", getBlocks);
+router.get("/", getBlocksById);
 
 module.exports = router;
