9e55db78e606f2ed138b8465f9677b8dba38a5a4
Semonati <93730539+Semonati@users.noreply.github.com>
2024-07-15 14:38:26 +0300

    Update router.js

--- server/router/router.js
+++ server/router/router.js
@@ -1,11 +1,23 @@
 const express = require("express");
 // const blocksControllers = require("../controllers/blockController");
+const Blocks = require("../DB/mongodb/blocks");
+const DB = process.env.DB || "MONGODB";
 const { getBlocks, getBlocksById } = require("../controllers/blockController");
 
 const router = express.Router();
 
 // router.use("/",blocksControllers);
-router.get("/", getBlocks);
-router.get("/:id", getBlocksById);
-
+// router.get("/", getBlocks);
+// router.get("/:id", getBlocksById);
+router.get("/", async (req, res) => {
+  if (DB === "MONGODB") {
+    try {
+      const blocks = await Blocks.find();
+      res.status(200).send(blocks);
+    } catch (error) {
+      error.status = 404;
+      return Promise.reject(error);
+    }
+  }
+});
 module.exports = router;
